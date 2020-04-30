4732b2728059fb09deddd37c79a0084cc362f757
Igor Gotlibovych <igor.gotlibovych@gmail.com>
2020-04-30 14:37:14 +0100

    Fix s3 prefix

--- src/s3migrate/api.py
+++ src/s3migrate/api.py
@@ -13,15 +13,16 @@ logger = logging.getLogger(__name__)
 __all__ = ["cp", "copy", "mv", "move", "rm", "remove", "iter", "iterformats"]
 
 
-fs = s3fs.S3FileSystem()
+FS = s3FS.S3FileSystem()
+PROTOCOL = "s3"
 
 
 def _yield_candidates(fmt_in):
     base_path = immutable_base(fmt_in)
     logger.info("Looking for objects in %s", base_path)
-    for (dirpath, dirnames, filenames) in fs.walk(base_path):  # FixMe: once tio is updated
+    for (dirpath, dirnames, filenames) in FS.walk(base_path):  # FixMe: once tio is updated
         for filename in filenames:
-            yield fs.protocol + "://" + dirpath + fs.sep + filename
+            yield PROTOCOL + "://" + dirpath + FS.sep + filename
 
 
 def iterformats(fmt_in):
@@ -49,7 +50,7 @@ def copy(fmt_in, fmt_out, dryrun=True):
         path_out = fmt_out.format(**fmt)
         logger.debug("Copying %s to %s", path_in, path_out)
         if not dryrun:
-            fs.copy_basic(path_in, path_out)
+            FS.copy_basic(path_in, path_out)
 
 
 def move(fmt_in, fmt_out, dryrun=True):
@@ -58,7 +59,7 @@ def move(fmt_in, fmt_out, dryrun=True):
         path_out = fmt_out.format(**fmt)
         logger.debug("Moving %s to %s", path_in, path_out)
         if not dryrun:
-            fs.move(path_in, path_out)
+            FS.move(path_in, path_out)
 
 
 def remove(fmt_in, dryrun=True):
@@ -66,7 +67,7 @@ def remove(fmt_in, dryrun=True):
         path_in = fmt_in.format(**fmt)
         logger.debug("Removing %s", path_in)
         if not dryrun:
-            fs.rm(path_in)
+            FS.rm(path_in)
 
 
 cp = copy
